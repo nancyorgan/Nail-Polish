6dd7a9731e85189bef260abea7c5d98cc632d3e4
Nancy Organ <nancyorgan@nancyorgansmbp.lan>
2019-10-28 20:59:14 -0700

    Starting Dior

--- nailpolish.R
+++ nailpolish.R
@@ -4,6 +4,7 @@ library("rgl")
 library("misc3d")
 library("data.table")
 library("RImagePalette")
+library("png")
 # install_github("andreacirilloac/paletter")
 
 
@@ -11,7 +12,6 @@ url <- list(
   Revlon = 'https://www.revlon.com/nails/nail-color/revlon-nail-enamel?shade=adventurous',
   Maybelline = 'https://www.maybelline.com.au/nail-makeup/nail-color/color-show-60-seconds-nail-lacquer/',
   Maybelline2 = 'https://www.maybelline.com.au/nail-makeup/nail-color/super-stay-7-days',
-  Orosa = 'https://orosabeauty.com/products/pre-fall-set',
   NARS = 'https://www.narscosmetics.com/USA/milos-nail-polish/0607845036456.html?gclid=Cj0KCQjwl8XtBRDAARIsAKfwtxBiqFJw74nAgsr_bolu-Uojx7H7jBq-E6rET3DwUlBgaMudndeL96EaAkBtEALw_wcB&gclsrc=aw.ds',
   Essie = 'https://www.essie.com/nail-polish/by-color/',
   Dior = 'https://www.dior.com/en_us/products/beauty-Y0002959_F000355257-dior-vernis-couture-color-gel-shine-long-wear-nail-lacquer?gclid=Cj0KCQjwl8XtBRDAARIsAKfwtxAvCXrpqEGt2MfLk3Ta3xJMNDGAExmKGCkqlJ_RYPl-16DUg58BdpwaAj1pEALw_wcB&gclsrc=aw.ds',
@@ -19,7 +19,8 @@ url <- list(
   YSL = 'https://www.yslbeautyus.com/makeup/nails/la-laque-couture/1023YSL.html',
   Urban = 'https://www.urbanoutfitters.com/shop/uo-nail-polish',
   Jhannah = 'https://jhannahjewelry.com/collections/nailpolish',
-  Louboutin = 'http://us.christianlouboutin.com/us_en/pluminette.html'
+  Louboutin = 'http://us.christianlouboutin.com/us_en/pluminette.html',
+  Orosa = 'https://orosabeauty.com/products/pre-fall-set',
   )
 
 ######################################################################
@@ -118,22 +119,6 @@ essie = function(url){
       )
     }
     
-    
-    # for (indx in 1:nrow(df_nested)) {
-    #   tryCatch(
-    #     expr = {
-    #       df_nested[[indx, "data"]] <-  df_nested[[indx, "data"]] %>% 
-    #         convert_gear_to_factors() %>% 
-    #         transform_response_to_log()
-    #       message("Iteration ", indx, " successful.")
-    #     },
-    #     error = function(e){
-    #       message("* Caught an error on itertion ", indx)
-    #       print(e)
-    #     }
-    #   )
-    # }
-    
     allData[[i]] = data.frame(group = groups[i], name = names, palette = palette)
   
   }
@@ -143,8 +128,57 @@ essie = function(url){
 
 # essieData = essie(url$Essie)
 
-write.csv(essieData, "/Users/nancyorgan/Desktop/essiedata.csv", row.names = FALSE)
+# write.csv(essieData, "/Users/nancyorgan/Desktop/essiedata.csv", row.names = FALSE)
 
+######################################################################
+######################################################################
+
+dior = function(url){
+  
+    webpage = read_html(url$Dior)
+    
+    names = webpage %>% html_nodes(css = 'label.sr-only') 
+    names = gsub("</label>", "", names)
+    names = gsub("^<label.*>", "", names)
+    names = gsub("^[0-9]{3}.", "", names)
+    
+    colors = webpage %>% 
+      html_nodes(css = "span.swatch") %>% 
+      html_nodes("img") %>% 
+      html_attr("src")
+    
+    palette = NULL
+    for(i in 1:length(colors)){
+      tryCatch(
+        expr = {
+          extension = substr(colors[i], nchar(colors[i]) - 3, nchar(colors[i]))
+          temp.file = ifelse(extension == ".png", 
+                             tempfile(fileext = ".png"),
+                             tempfile(fileext = ".jpg"))
+        
+          download.file(paste0("https://www.dior.com/", colors[i]), destfile = temp.file)
+          img = ifelse(extension == ".png", 
+                       readPNG(temp.file),
+                       readJPEG(temp.file))
+          
+          palette[i] = image_palette(img, n=1)
+          unlink(temp.file)
+        },
+        error = function(e){
+          message("* Caught an error on itertion ", i)
+          print(e)
+        }
+      )
+    }
+    
+    allData = data.frame(names = names, colors = color)
+  }
+  return(allData)
+}
+
+dior = essie(url$Dior)
+
+# write.csv(essieData, "/Users/nancyorgan/Desktop/essiedata.csv", row.names = FALSE)
 
 ######################################################################
 ######################################################################
